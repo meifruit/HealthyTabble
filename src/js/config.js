d3fdeca77f3f62302569309dea27556b50ba9ef0
Xuemei Huang <mei.huang9500@gmail.com>
2023-09-28 22:45:25 +0900

    add file

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fb9640d2-075e-4a4f-a605-2baa8d328d0f';
+export const MODAL_CLOSE_SEC = 2.5;
